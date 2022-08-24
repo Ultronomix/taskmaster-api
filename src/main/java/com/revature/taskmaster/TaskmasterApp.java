e0be1fb16d002e821dda3849c8bbfb8742e73057
Wezley Singleton <wezley.singleton@gmail.com>
2022-08-24 16:03:30 -0400

    brief example showing how to make a JDBC connection to a DB

--- src/main/java/com/revature/taskmaster/TaskmasterApp.java
+++ src/main/java/com/revature/taskmaster/TaskmasterApp.java
@@ -1,8 +1,25 @@
 package com.revature.taskmaster;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TaskmasterApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String dbUrl = "jdbc:postgresql://fake-db-url:5432/postgres?currentSchema=taskmaster";
+        String dbUsername = "fake-user";
+        String dbPassword = "fake-password";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            if (conn != null) {
+                System.out.println("Connection successful!");
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Could not find PostgreSQL JDBC driver. Connection attempt aborted.");
+        } catch (SQLException e) {
+            System.err.println("Could not establish a connection to the database.");
+        }
     }
 }
