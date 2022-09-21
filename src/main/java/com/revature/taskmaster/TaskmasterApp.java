197699bcfb1828ea7ef08a38f06bb1b0a70d503b
Wezley Singleton <wezley.singleton@gmail.com>
2022-09-21 12:06:08 -0400

    using Anno Web App Context for bean container, registered DispatcherServlet to foward reqs to controllers; pending UserServlet conversion -> UserController

--- src/main/java/com/revature/taskmaster/TaskmasterApp.java
+++ src/main/java/com/revature/taskmaster/TaskmasterApp.java
@@ -8,6 +8,8 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class TaskmasterApp {
 
@@ -17,31 +19,31 @@ public class TaskmasterApp {
 
         logger.info("Starting Taskmaster application");
 
-        try (AnnotationConfigApplicationContext beanContainer = new AnnotationConfigApplicationContext(AppConfig.class)) {
 
-            String docBase = System.getProperty("java.io.tmpdir");
-            Tomcat webServer = new Tomcat();
+        String docBase = System.getProperty("java.io.tmpdir");
+        Tomcat webServer = new Tomcat();
 
-            // Web server base configurations
-            webServer.setBaseDir(docBase);
-            webServer.setPort(5000); // defaults to 8080, but we can set it to whatever port we want (as long as its open)
-            webServer.getConnector(); // formality, required in order for the server to receive requests
+        // Web server base configurations
+        webServer.setBaseDir(docBase);
+        webServer.setPort(5000); // defaults to 8080, but we can set it to whatever port we want (as long as its open)
+        webServer.getConnector(); // formality, required in order for the server to receive requests
+        final String rootContext = "/taskmaster";
+        webServer.addContext(rootContext, docBase);
 
-            UserServlet userServlet = beanContainer.getBean(UserServlet.class);
-            AuthController authController = beanContainer.getBean(AuthController.class);
+        AnnotationConfigWebApplicationContext beanContainer = new AnnotationConfigWebApplicationContext();
+        beanContainer.register(AppConfig.class);
 
-            // Web server context and servlet configurations
-            final String rootContext = "/taskmaster";
-            webServer.addContext(rootContext, docBase);
-            webServer.addServlet(rootContext, "UserServlet", userServlet).addMapping("/users");
-            webServer.addServlet(rootContext, "AuthServlet", authController).addMapping("/auth");
+        //UserServlet userServlet = beanContainer.getBean(UserServlet.class);
 
-            // Starting and awaiting web requests
-            webServer.start();
-            logger.info("Taskmaster web application successfully started");
-            webServer.getServer().await();
+        // Web server context and servlet configurations
 
-        }
+        //webServer.addServlet("/v1", "UserServlet", userServlet).addMapping("/users");
+        webServer.addServlet(rootContext, "DispatcherServlet", new DispatcherServlet(beanContainer)).addMapping("/");
+
+        // Starting and awaiting web requests
+        webServer.start();
+        logger.info("Taskmaster web application successfully started");
+        webServer.getServer().await();
 
     }
 
