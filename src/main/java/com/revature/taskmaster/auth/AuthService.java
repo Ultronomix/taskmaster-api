39d62640d6fa5bed5fee50e59fd05d7a7862e5b8
Wezley Singleton <wezley.singleton@gmail.com>
2022-09-21 11:24:12 -0400

    added Autowired anno to constructor of AuthService

--- src/main/java/com/revature/taskmaster/auth/AuthService.java
+++ src/main/java/com/revature/taskmaster/auth/AuthService.java
@@ -4,6 +4,7 @@ import com.revature.taskmaster.common.exceptions.AuthenticationException;
 import com.revature.taskmaster.common.exceptions.InvalidRequestException;
 import com.revature.taskmaster.users.UserDAO;
 import com.revature.taskmaster.users.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class AuthService {
 
     private final UserDAO userDAO;
 
+    @Autowired
     public AuthService(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
