f52ef38b9bce6f9833c6e4f133f3f6b1907806bd
Wezley Singleton <wezley.singleton@gmail.com>
2022-09-23 11:04:51 -0400

    added dependency for open api/swagger rest api documentation

--- src/main/java/com/revature/taskmaster/TaskmasterApp.java
+++ src/main/java/com/revature/taskmaster/TaskmasterApp.java
@@ -1,20 +1,13 @@
 package com.revature.taskmaster;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication // implies @Configuration @ComponentScan and @AutoConfiguration
 public class TaskmasterApp {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskmasterApp.class, args);
-    }
-
-    @Bean
-    public ObjectMapper jsonMapper() {
-        return new ObjectMapper();
     }
 
 }
