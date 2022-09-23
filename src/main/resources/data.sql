ea98062c44616670a71e4e92566d79c6893ced34
Wezley Singleton <wezley.singleton@gmail.com>
2022-09-23 12:50:37 -0400

    adding mock user in data.sql; updated build.yml to use test profile

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,6 @@ INSERT INTO user_roles VALUES ('98e78bd57dcf475d901f4b4072651999', 'LEAD');
 INSERT INTO user_roles VALUES ('4b4121d715524cd0995dfa8e5618e154', 'SENIOR');
 INSERT INTO user_roles VALUES ('5a2e0415ee08440fab8a778b37ff6874', 'JUNIOR');
 INSERT INTO user_roles VALUES ('c2dfbb5fb4b3485284db146984bc6a0b', 'QA');
+
+INSERT INTO app_users (id, username, password, given_name, surname, email, role_id)
+VALUES ('0cf6b1e0-a092-49df-b553-aebcf70d40b4', 'someone', 'p4$$w0RD', 'Some', 'One', 'someone@revature.com', '5a2e0415ee08440fab8a778b37ff6874');
